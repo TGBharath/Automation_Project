9530171a61b3af152ed7e47f3cc2d760cc4760e0
TGBharath <108675637+TGBharath@users.noreply.github.com>
2022-08-02 00:29:05 +0530

    arranging script lines

--- Automation.sh
+++ Automation.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-sudo apt update -y
+myname=Bharath
 apache2=$(apache2 -v)
+apache2status=$( sudo systemctl status apache2 )
+timestamp=$(date '+%d%m%Y-%H%M%S')
+s3_bucket='upgrad-bharathtg'
+
+sudo apt update -y
 
 if [[ $apache2 == *"Apache"* ]]; then
-    echo "apache2 is present"
+	echo "apache2 is present"
 else sudo apt install apache2
 fi
 
-servstat=$( sudo systemctl status apache2 )
-
-if [[ $servstat == *"inactive (dead)"* ]]; then
+if [[ $apache2status == *"inactive (dead)"* ]]; then
 	echo "staring apache2 service"
 	sudo systemctl start apache2
 else echo "apache2 service is running"
 fi
 
-myname=Bharath
-timestamp=$(date '+%d%m%Y-%H%M%S')
-s3_bucket='upgrad-bharathtg'
 cd /tmp
+
 tar -cvf ${myname}-httpd-logs-${timestamp}.tar /var/log/apache2
 
 aws s3 \
