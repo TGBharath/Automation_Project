c5e3d3d57f378a399780d3b2da73dd79ba0488d2
TGBharath <108675637+TGBharath@users.noreply.github.com>
2022-08-03 02:08:15 +0530

    added script for cron and bookkeeping

--- automation.py
+++ automation.py
@@ -1,27 +1,62 @@
 #!/bin/bash
 
-sudo apt update -y
 apache2=$(apache2 -v)
+apache2_status=$( sudo systemctl status apache2 )
+myname=Bharath
+s3_bucket='upgrad-bharathtg'
+cron_file='/etc/cron.d/automation'
+bookkeeping='/var/www/html/inventory.html'
+size=$(stat -c %s /tmp/${myname}-httpd-logs-${timestamp}.tar)
+
+#to update packages
+
+sudo apt update -y
+
+
+#to check apache2 service
 
 if [[ $apache2 == *"Apache"* ]]; then
     echo "apache2 is present"
 else sudo apt install apache2
 fi
 
-servstat=$( sudo systemctl status apache2 )
-
-if [[ $servstat == *"inactive (dead)"* ]]; then
-	echo "staring apache2 service"
-	sudo systemctl start apache2
+if [[ $apache2_status == *"inactive (dead)"* ]]; then
+        echo "staring apache2 service"
+        sudo systemctl start apache2
 else echo "apache2 service is running"
 fi
 
-myname=Bharath
+
+#to upload log files to s3 bucket
+
 timestamp=$(date '+%d%m%Y-%H%M%S')
-s3_bucket='upgrad-bharathtg'
-cd /tmp
-tar -cvf ${myname}-httpd-logs-${timestamp}.tar /var/log/apache2
+
+tar -czvf /tmp/${myname}-httpd-logs-${timestamp}.tar /var/log/apache2/*.log
 
 aws s3 \
 cp /tmp/${myname}-httpd-logs-${timestamp}.tar \
 s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
+
+
+#to cron the script
+
+if test -f "$cron_file" 
+then
+        echo "cron exists"
+else
+        touch $cron_file
+        echo -e "SHELL=/bin/bash\nPATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin \n* 1 * * * root /Automation_Project/automation.sh" >> $cron_file
+fi
+
+
+#to bookkeeping 
+
+if test -f "$bookkeeping"
+then
+        echo "inventory.html exists"
+else
+        touch $bookkeeping
+        echo -e "<table style="width:50%">\n <tr>\n  <th>Log Type</th>\n  <th>Time Created</th>\n  <th>Type</th>\n  <th>Size</th>\n </tr>\n</table>" >> $bookkeeping
+fi
+
+echo -e "<table style="width:50%">\n <tr>\n  <td>httpd-logs</td>\n  <td>$timestamp</td>\n  <td>tar</td>\n  <td>$size</td>\n </tr>\n</table>" >> $bookkeeping
